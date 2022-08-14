91522e6bf7dd5a48902a03267472aeaa86c700c4
SNRainiar <49141130+SamuNatsu@users.noreply.github.com>
2022-08-14 21:57:03 +0800

    Add files via upload

--- shared/404.php
+++ shared/404.php
@@ -6,7 +6,21 @@ if (!defined('__SSF__')) exit;
 <html>
 <head>
 	<title><?php \SSF\Options::title('404', true); ?></title>
+	<link rel="stylesheet" href="<?php \SSF\Router::shared('/modern-normalize.min.css', true); ?>"/>
+	<link rel="stylesheet" href="<?php \SSF\Router::shared('/flex.css', true); ?>"/>
+	<style>
+		body {color:gray;font-weight:bold;height:100%;position:absolute;width:100%}
+		.txt-1 {font-size:8em}
+		.txt-2 {font-size:2em}
+	</style>
 </head>
-<body>
+<body class="flex-box flex-x-center flex-mid">
+	<div class="flex-box flex-baseline">
+		<div class="txt-1">404</div>
+		<div class="flex-box flex-rev-col">
+			<div class="txt-2">Found</div>
+			<div class="txt-2">Not</div>
+		</div>
+	<div>
 </body>
 </html>
